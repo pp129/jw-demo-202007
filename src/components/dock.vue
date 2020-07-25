aa7844f53bfc1146567ba25b7c2a196eff3352d9
PanFei <604042454@qq.com>
2020-07-25 18:07:05 +0800

    v07251800

---
+++ src/components/dock.vue
@@ -0,0 +1,120 @@
+<template>
+	<div>
+		<div class="dock"></div>
+		<div class="dock-box">
+			<div
+				v-for="item in docks"
+				:key="item.link"
+				class="dock-items"
+				:class="item.active ? 'dock-items-active' : ''"
+				@click="changeView(item)"
+			>
+				<span>{{ item.content }}</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+  import _ from "lodash";
+  export default {
+    name: 'dock',
+    data() {
+      return {
+        docks: [
+          {
+            name: "About",
+            content: "业务简介",
+            link: "About",
+            active: true
+          },
+          {
+            name: "ClassicCase",
+            content: "典型案例",
+            link: "ClassicCase",
+            active: false
+          },
+          {
+            name: "HonoraryAndCertificate",
+            content: "荣誉资质",
+            link: "/HonoraryAndCertificate",
+            active: false
+          }
+        ]
+      };
+    },
+    mounted() {
+      this.setActive();
+    },
+    methods: {
+      setActive(name) {
+        const link = this.$route;
+        _.each(this.docks, e => {
+          e.active = false;
+        });
+        _.find(this.docks, { name: name ? name : link.name }).active = true;
+      },
+      changeView(item) {
+        _.each(this.docks, e => {
+          e.active = false;
+        });
+        this.$router.push(item.link);
+        item.active = true;
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" type="text/scss">
+	@import "~@/style/var.scss";
+	.dock {
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		width: (630px * 100vw / $screenWidth);
+		height: (82px * 100vh / $screenHeight);
+		margin-left: (315px * 100vw / $screenWidth);
+		background: url("~@/assets/img/bottom_lights.png") no-repeat;
+		background-size: 100% 100%;
+	}
+	.dock-box {
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		width: (435px * 100vw / $screenWidth);
+		margin-left: (217.5px * 100vw / $screenWidth);
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		.dock-items {
+			width: (167px * 100vw / $screenWidth);
+			height: (51px * 100vh / $screenHeight);
+			cursor: pointer;
+			background: url("~@/assets/img/icon-dock.png") no-repeat center 25px;
+			text-align: center;
+			span {
+				font-size: 3.2rem;
+				font-family: NotoSansHans-Regular, serif;
+				font-weight: 400;
+				color: rgba(0, 180, 241, 1);
+				line-height: 6.4rem;
+			}
+			&-active {
+				width: (167px * 100vw / $screenWidth);
+				height: (51px * 100vh / $screenHeight);
+				background: url("~@/assets/img/icon-dock-a.png") no-repeat center -20px;
+				span {
+					color: #fff;
+				}
+			}
+			&:hover {
+				width: (167px * 100vw / $screenWidth);
+				height: (51px * 100vh / $screenHeight);
+				background: url("~@/assets/img/icon-dock-a.png") no-repeat center -20px;
+				span {
+					color: #fff;
+				}
+			}
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,19 @@
     <div class="box">
       面向公安、交警、安防、军营等领域，基于北斗/GPS、物联网、云计算、5G、大数据和人工智能等技术， 汇聚警员、警情、警备、勤务、视频、事件等全要素警务资源，提供勤务实战、交通安保、治安防控、便民服务、大数据分析研判等多个方面的解决方案和信息服务。构建高度集成、高度共享、高度智能的智慧警务新格局，全面实现警务工作信息化、智能化。
     </div>
+    <div class="menu menu-gongan"></div>
+    <div class="menu menu-jiaojing"></div>
+    <div class="menu menu-anfang"></div>
+    <div class="menu menu-junying"></div>
+    <dock></dock>
   </div>
 </template>
 <script>
-
+import dock from '@/components/dock'
 export default {
   name: 'Home',
   components: {
+    dock: dock
   }
 }
 </script>
@@ -62,5 +68,46 @@ export default {
       line-height: (58px * 100vh / $screenHeight);
       box-sizing: border-box;
     }
+    .menu{
+      width: 182px;
+      height: 228px;
+      position: absolute;
+      &-gongan{
+        background: url("~@/assets/img/智慧公安.png") no-repeat;
+        background-size: 100% 100%;
+        top: (240px * 100vh / $screenHeight);
+        left: (165px * 100vw / $screenWidth);
+        animation: heart1 3s ease-in-out 0s infinite alternate;
+      }
+      &-jiaojing{
+        background: url("~@/assets/img/智慧交警.png") no-repeat;
+        background-size: 100% 100%;
+        top: (240px * 100vh / $screenHeight);
+        right: (153px * 100vw / $screenWidth);
+        animation: heart1 3s ease-in-out 0s infinite alternate;
+      }
+      &-anfang{
+        background: url("~@/assets/img/智慧安防.png") no-repeat;
+        background-size: 100% 100%;
+        bottom: (160px * 100vh / $screenHeight);
+        left: (280px * 100vw / $screenWidth);
+        animation: heart2 3s ease-in-out 0s infinite alternate;
+      }
+      &-junying{
+        background: url("~@/assets/img/智慧军营.png") no-repeat;
+        background-size: 100% 100%;
+        bottom: (160px * 100vh / $screenHeight);
+        right: (244px * 100vw / $screenWidth);
+        animation: heart2 3s ease-in-out 0s infinite alternate;
+      }
+    }
+  }
+  @keyframes heart1{
+    from{transform:translate(0,-5vh)}
+    to{transform:translate(0,5vh)}
+  }
+  @keyframes heart2{
+    from{transform:translate(0,5vh)}
+    to{transform:translate(0,-5vh)}
   }
 </style>
